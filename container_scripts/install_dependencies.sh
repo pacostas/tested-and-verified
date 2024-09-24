3637cda6977db44b637584051635156e65673cd6
Costas Papastathis <papastathiscr@gmail.com>
2024-09-24 14:33:47 +0300

    fixing ubuntu

--- container_scripts/install_dependencies.sh
+++ container_scripts/install_dependencies.sh
@@ -5,7 +5,7 @@ OS_NAME=$2
 
 if [ "$OS_NAME" = "ubuntu" ]; then
     curl -fsSL "https://deb.nodesource.com/setup_${NODE_VERSION}.x" | bash - &&
-        apt-get install -y nodejs git jq
+        apt-get -y update && apt-get install -y nodejs git jq curl npm
 elif [ "$OS_NAME" = "rhel" ]; then
     yum -y module enable "nodejs:${NODE_VERSION}" &&
         yum -y install git nodejs nodejs-nodemon npm jq && yum clean all
