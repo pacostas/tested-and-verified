a9c594d532bf1b058f58a536f4408d8c10acf1c4
Costas Papastathis <papastathiscr@gmail.com>
2024-11-18 16:11:52 +0200

    iterating over oss

--- .github/scripts/get_supported_modules_and_node_versions.sh
+++ .github/scripts/get_supported_modules_and_node_versions.sh
@@ -2,7 +2,7 @@
 
 node_module=$1
 node_versions=$2
-os=$3
+oss=$3
 repeat_n_times=$4
 
 # Fetch the supported modules in case of all input
@@ -12,28 +12,31 @@ else
   node_modules=$node_module
 fi
 
-for num in $(seq 1 $repeat_n_times); do
+nums=$(seq 1 $repeat_n_times)
+for num in $nums; do
   for module in $node_modules; do
+    for os in $oss; do
 
-    skip_versions=($(jq -r ".$module.skip.$os.node_versions[]?" ./supported_modules.json))
+      skip_versions=($(jq -r ".$module.skip.$os.node_versions[]?" ./supported_modules.json))
 
-    for node_version in $node_versions; do
+      for node_version in $node_versions; do
 
-      skip=false
-      for skip_version in "${skip_versions[@]}"; do
-        if [[ "$node_version" == "$skip_version" ]]; then
-          skip=true
-          break
+        skip=false
+        for skip_version in "${skip_versions[@]}"; do
+          if [[ "$node_version" == "$skip_version" ]]; then
+            skip=true
+            break
+          fi
+        done
+        if $skip; then
+          continue
         fi
-      done
-      if $skip; then
-        continue
-      fi
 
-      module_node_versions+=("{\"job\": \"${module}_${node_version}\", \"name\": \"$module\",  \"repeat_num\": \"$num\", \"node_version\": \"$node_version\"}")
+        module_os_node+=("{\"job\": \"${module}_${os}_${node_version}\", \"name\": \"$module\", \"repeat_num\": \"$num\", \"node_version\": \"$node_version\", \"os\": \"$os\"}")
+      done
     done
   done
 done
 
-json_module_node_version=$(printf '%s\n' "${module_node_versions[@]}" | jq -R . | jq -s . | jq -c .)
-echo "module_node_version=$json_module_node_version"
+json_module_node_version=$(printf '%s\n' "${module_os_node[@]}" | jq -R . | jq -s . | jq -c .)
+echo "module_os_node=$json_module_node_version"
